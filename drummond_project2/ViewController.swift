ac3a25dba05f0acdf302b5f066e26d8ea2d06d41
Kyle Drummond <krd48@254-15.cs.uakron.edu>
2018-02-28 16:36:22 -0500

    Implemented reference outlet so user can hit return button on keyboard and have the keyboard disappear

--- drummond_project2/ViewController.swift
+++ drummond_project2/ViewController.swift
@@ -11,48 +11,19 @@ class ViewController: UIViewController, UITextFieldDelegate {
     // email address entry
     // Note: Must create text field in storyboard, and connect to this IBOutlet
     @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var first: UITextField!
+    @IBOutlet weak var last: UITextField!
+    @IBOutlet weak var restaurant: UITextField!
+    //@IBOutlet weak var f_name: UITextField!
     
-    @IBOutlet weak var f_name: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.f_name.text = "matt"
-        
         self.emailText.delegate = self
-        
-        // shifts the view up for space for the keyboard
-        NotificationCenter.default.addObserver(forName: .UIKeyboardWillShow, object: nil, queue: nil) {
-            notification in
-            
-            // find the size of the keyboard
-            guard let keyboardSize = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
-                print("ERROR: Unable to get keyboard size")
-                return
-            }
-            
-            // move the whole thing up
-            self.view.frame.origin.y = -keyboardSize.height
-        }
-        
-        // shifts the view up for space for the keyboard
-        NotificationCenter.default.addObserver(forName: .UIKeyboardWillHide, object: nil, queue: nil) {
-            notification in
-            
-            // move back to original position
-            self.view.frame.origin.y = 0
-        }
+        self.first.delegate = self
+        self.last.delegate = self
+        self.restaurant.delegate = self
     }
-    
-    func loadDefaults() {
-        // TODO: Load in plist values
-    }
-    
-    @IBAction func updateDefaults() {
-        // TODO: Grab the input field values and update the plist with them
-        print("we just saved the user inputs.")
-    }
-    
     // Make the keyboard disappear once return is pressed
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         
@@ -61,4 +32,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
         // Note: implements default return key behavior
         return true
     }
+    
+
 }
+
+
