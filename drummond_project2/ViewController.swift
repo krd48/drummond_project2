e703fac946d9c9912a385d5f8446c5b6c16c4e2d
Kyle Drummond <krd48@254-15.cs.uakron.edu>
2018-02-28 18:27:11 -0500

    Implemented ability to save data to plist for 1 text field

--- drummond_project2/ViewController.swift
+++ drummond_project2/ViewController.swift
@@ -1,12 +1,22 @@
-//
+// Kyle Drummond
+// IOS Project 2 SP 2018
 // Moving keyboard for item
 //
 // Note: Comments with this label are to explain semantics of iOS
 //       Do not use comments like these in your projects
-
+// Used the class github account as a reference
 import UIKit
 
+struct DefaultInfo : Codable{
+    var First_name: String?
+}
+
 class ViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    // mood of the (single) user
+    var defaultInfo = DefaultInfo()
+    
     
     // email address entry
     // Note: Must create text field in storyboard, and connect to this IBOutlet
@@ -14,26 +24,108 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var first: UITextField!
     @IBOutlet weak var last: UITextField!
     @IBOutlet weak var restaurant: UITextField!
+    
     //@IBOutlet weak var f_name: UITextField!
     
+    @IBOutlet weak var firstname: UITextField!
+    @IBOutlet weak var moodText: UITextField!
+
     
-    override func viewDidLoad() {
+    // URL for file UserData.plist in user document directory for this app
+    lazy var localFileURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("Default_Info").appendingPathExtension("plist")
+    
+   override func viewDidLoad() {
         super.viewDidLoad()
         self.emailText.delegate = self
         self.first.delegate = self
         self.last.delegate = self
         self.restaurant.delegate = self
+    
+    
+    self.emailText.delegate = self
+
+    
+    defer {
+    // set the default field
+    self.emailText.text = self.defaultInfo.First_name
     }
-    // Make the keyboard disappear once return is pressed
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        
-        textField.resignFirstResponder()
-        
-        // Note: implements default return key behavior
-        return true
+
+    guard let defaultInfoURL = Bundle.main.url(forResource: "project2", withExtension:"plist") else {
+    print("Error: Unable to form path")
+    return
     }
     
-
+    guard let data = try? Data(contentsOf:defaultInfoURL) else {
+        print("Error: Invalid Data")
+        return
+    }
+    
+    // convert the class data plist to a classRecord
+    guard let defaultInfo = try? PropertyListDecoder().decode(DefaultInfo.self, from: data) else {
+        print("Error: Bad data format for property list")
+        return
+    }
+    
+    self.defaultInfo = defaultInfo
+    
+    /* Read user data from a plist file in an user's document directory */
+    
+    // data in resource file
+    guard let localData = try? Data(contentsOf: defaultInfoURL) else {
+        print("Error: Invalid Data")
+        return
+    }
+    
+    // convert the class data plist to a [String:String] dictionary
+    guard let localDefaultInfoData = try? PropertyListDecoder().decode(DefaultInfo.self, from: localData) else {
+        print("Error: Bad data format for property list")
+        return
+    }
+    
+    self.defaultInfo = localDefaultInfoData
 }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+     textField.resignFirstResponder()
+        
+        self.emailText.resignFirstResponder()
+        
+        DispatchQueue.main.async {
+            if let First_name = self.emailText.text {
+                print("New name: \(First_name)")
+                
+                self.defaultInfo.First_name = First_name
+                
+                // save the userData to the userDataURL
+                let encoder = PropertyListEncoder()
+                encoder.outputFormat = .xml
+                do {
+                    try encoder.encode(self.defaultInfo).write(to: self.localFileURL)
+                } catch {
+                    print(error)
+                }
+                
+                print("\(#file), \(#function), \(#line) \(self.defaultInfo)")
+                
+                print("User data file: \(self.localFileURL)")
+            }
+        }
+        
+        return true
+    }
+}
+    
+//
+//    // Make the keyboard disappear once return is pressed
+//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//
+//
+//
+//        // Note: implements default return key behavior
+//        return true
+//        }
+//
+//    }
 
 
