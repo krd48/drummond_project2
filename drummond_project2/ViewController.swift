5f005550a1d2c0a4d1d131717da9e9d17ba508f5
Kyle Drummond <krd48@254-15.cs.uakron.edu>
2018-02-28 20:23:41 -0500

    output switch button text to console

--- drummond_project2/ViewController.swift
+++ drummond_project2/ViewController.swift
@@ -26,6 +26,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
         else{
             output.text = "No I am not hungry"
         }
+        print(output)
     }
     
     // mood of the (single) user
